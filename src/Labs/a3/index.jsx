d989d50bbdb4491ad77dbad2240bfeed8e444409
Richard Beaver <beaver.r@northeastern.edu>
2023-10-13 18:10:32 -0400

    Create todo list (end of lab 3)

--- src/Labs/a3/index.jsx
+++ src/Labs/a3/index.jsx
@@ -3,11 +3,16 @@ import ConditionalOutput from "./ConditionalOutput";
 import JavaScript from "./JavaScript";
 import PathParameters from "./PathParameters";
 import Styles from "./Styles";
+import TodoItem from "./Todos/TodoItem";
+import TodoList from "./Todos/TodoList";
 
 function Assignment3() {
   return (
     <div className="container">
       <h1>Assignment 3</h1>
+
+      <TodoList />
+      <TodoItem />
 
       <ConditionalOutput />
       <Styles />
