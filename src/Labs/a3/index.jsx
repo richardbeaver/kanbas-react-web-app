d5fddcb2c5086d265a98131ddc12ed4fe77d3a2f
Richard Beaver <beaver.r@northeastern.edu>
2023-10-13 18:01:24 -0400

    Syling and conditional output

--- src/Labs/a3/index.jsx
+++ src/Labs/a3/index.jsx
@@ -1,11 +1,17 @@
+import Classes from "./Classes";
+import ConditionalOutput from "./ConditionalOutput";
 import JavaScript from "./JavaScript";
 import PathParameters from "./PathParameters";
+import Styles from "./Styles";
 
 function Assignment3() {
   return (
     <div className="container">
       <h1>Assignment 3</h1>
 
+      <ConditionalOutput />
+      <Styles />
+      <Classes />
       <PathParameters />
       <JavaScript />
     </div>
