ed039e6d1950a062ca3fa4360d9df1440760b107
Richard Beaver <beaver.r@northeastern.edu>
2023-10-19 14:24:13 -0400

    Create Dashboard

---
+++ src/Kanbas/Dashboard/Card.jsx
@@ -0,0 +1,44 @@
+import { BiSolidBook } from "react-icons/bi";
+import { FaEllipsisVertical } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
+const backgroundOptions = ["primary", "secondary", "success", "info"];
+
+function chooseCardBackground() {
+  let idx = Math.floor(Math.random() * backgroundOptions.length);
+  return backgroundOptions[idx];
+}
+
+function Card({ course }) {
+  const backgroundColor = chooseCardBackground();
+
+  return (
+    <div className="position-relative p-2">
+      <Link
+        to={`/Kanbas/Courses/${course._id}`}
+        className="text-decoration-none"
+      >
+        <div className="card">
+          <div className={`card-header bg-${backgroundColor}`}></div>
+          <div className="card-body pt-2 pb-2">
+            <h5 className={`card-title text-${backgroundColor}`}>
+              {course.name}
+            </h5>
+            <p className="card-text mb-0">{course.number}</p>
+            <p className="card-text mb-2">{`${course.startDate} - ${course.endDate}`}</p>
+            <BiSolidBook />
+          </div>
+        </div>
+      </Link>
+
+      {/* Added "5%" to Bootstrap $position-values Sass map to use here */}
+      <div className="btn-toolbar justify-content-end position-absolute top-5 end-5">
+        <button className="btn">
+          <FaEllipsisVertical style={{ color: "white" }} />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/Kanbas/Dashboard/index.jsx
@@ -0,0 +1,27 @@
+import db from "../Database";
+import Card from "./Card";
+import "./index.css";
+
+function Dashboard() {
+  const courses = db.courses;
+
+  return (
+    <div className="dashboard">
+      <div className="nav-bar mt-3 me-2 d-none d-md-block">
+        <h1 className="display-5 text-left">Dashboard</h1>
+      </div>
+      <hr />
+
+      <h4>Published Courses ({courses.length})</h4>
+      <hr />
+
+      <div className="d-flex flex-wrap">
+        {courses.map((course) => (
+          <Card key={course._id} course={course} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
