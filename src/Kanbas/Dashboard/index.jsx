56f012acf34d37f8278491473198f53c0adcb10e
Richard Beaver <rickybeaver3@gmail.com>
2023-11-02 14:02:17 -0400

    Make dashboard courses interactive

--- src/Kanbas/Dashboard/index.jsx
+++ src/Kanbas/Dashboard/index.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import db from "../Database";
 import Card from "./Card";
 import "./index.css";
+import { Link } from "react-router-dom";
 
 function Dashboard() {
-  const courses = db.courses;
+  const [courses, setCourses] = useState(db.courses);
+  const [course, setCourse] = useState({
+    name: "New Course",
+    number: "New Number",
+    startDate: "2023-09-10",
+    endDate: "2023-12-15",
+  });
+
+  const addNewCourse = () => {
+    setCourses([...courses, { ...course, _id: new Date().getTime() }]);
+  };
 
   return (
     <div className="wd-dashboard">
@@ -15,10 +27,44 @@ function Dashboard() {
       <h4>Published Courses ({courses.length})</h4>
       <hr />
 
-      <div className="d-flex flex-wrap">
+      <h5>Course</h5>
+      <input
+        value={course.name}
+        className="form-control"
+        onChange={(e) => setCourse({ ...course, name: e.target.value })}
+      />
+      <input
+        value={course.number}
+        className="form-control"
+        onChange={(e) => setCourse({ ...course, number: e.target.value })}
+      />
+      <input
+        value={course.startDate}
+        className="form-control"
+        type="date"
+        onChange={(e) => setCourse({ ...course, startDate: e.target.value })}
+      />
+      <input
+        value={course.endDate}
+        className="form-control"
+        type="date"
+        onChange={(e) => setCourse({ ...course, endDate: e.target.value })}
+      />
+      <button onClick={addNewCourse}>Add</button>
+
+      <div className="list-group d-flex flex-wrap">
         {courses.map((course) => (
-          <Card key={course._id} course={course} />
+          <Link
+            key={course._id}
+            to={`/Kanbas/Courses/${course._id}`}
+            className="list-group-item"
+          >
+            {course.name}
+          </Link>
         ))}
+        {/* {courses.map((course) => (
+          <Card key={course._id} course={course} />
+        ))} */}
       </div>
     </div>
   );
